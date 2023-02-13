5ee5f4eda6af5c3df9a15feb95d0dee9bce16a60
PastelPossum <iggyskywalker@gmail.com>
2023-02-13 12:38:42 +0100

    Add img and hero js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+dokument.addEventListener("DOMContentLaoded", function() {
 
     // slå dig løs her... 
 
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
+
+    let heroSection = document.querySelector(".hero");
+
+    hero.forEach((hero) => {
+
+        let topNav = document.createElement("section")
+        topNav.setAttribute("class", "top_nav")
+        let cube = document.createElement("img")
+        let menu = document.createElement("section")
+        menu.setAttribute("class", "profile")
+        let arrow = document.createElement("img")
+        let profileIcon = document.createElement("img")
+
+        let heroImg = document.createElement("img")
+
+    })
+
 
 }) // DOMContentLoaded slut
