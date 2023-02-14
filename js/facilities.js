9695aecfb9282572ffc412c0b0c3789e6807376a
PastelPossum <iggyskywalker@gmail.com>
2023-02-14 12:58:18 +0100

    Add facilities.js

---
+++ js/facilities.js
@@ -0,0 +1,25 @@
+let facilities = {
+    headline: "Making your facility known is our priority",
+    options : [
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/list-check.png",
+            headline: "Choose the type of stop",
+            text: "Select the type of stopover you are ordering from Equipped Area, Camping and Agricamping"
+        },
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/spellcheck.png",
+            headline: "Describe your structure",
+            text: "Describe thee property, location and nearby attractions"
+        },
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Text-Frame-Add.png",
+            headline: "Build your showcase",
+            text: "Add photos, videos and descriptions Define the price of each type of that make your propety truly"
+        },
+        {
+            icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Calendar-Today.png",
+            headline: "Prices and Availability",
+            text: "pitch and configure the availablity calendar."
+        },
+    ]
+}
