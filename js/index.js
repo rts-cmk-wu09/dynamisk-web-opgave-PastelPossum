9c1dee90e79815d7f832e7c0a4b4b72e09502366
PastelPossum <iggyskywalker@gmail.com>
2023-02-14 08:49:53 +0100

    Add hero.js and append hero img. DELETE IMAGES

--- js/index.js
+++ js/index.js
@@ -1,25 +1,41 @@
-dokument.addEventListener("DOMContentLaoded", function() {
+document.addEventListener("DOMContentLaoded", function() {
 
     // slå dig løs her... 
 
     //eksempel på at udskrive alle overskrifter i services i konsollen:
     services.forEach(service => console.log(service.headline))
 
-    let heroSection = document.querySelector(".hero");
+    // let heroSection = document.querySelector(".hero");
 
-    hero.forEach((hero) => {
+    // hero.forEach((hero) => {
 
-        let topNav = document.createElement("section")
-        topNav.setAttribute("class", "top_nav")
-        let cube = document.createElement("img")
-        let menu = document.createElement("section")
-        menu.setAttribute("class", "profile")
-        let arrow = document.createElement("img")
-        let profileIcon = document.createElement("img")
+    //     let topNav = document.createElement("section")
+    //     topNav.classList.add("top_nav")
+    //     let cube = document.createElement("img")
+    //     let menu = document.createElement("section")
+    //     menu.classList.add("profile")
+    //     let arrow = document.createElement("img")
+    //     let profileIcon = document.createElement("img")
 
-        let heroImg = document.createElement("img")
+    //     let heroImg = document.createElement("img")
 
-    })
+    //     let overBox = document.createElement("section")
+    //     let title = document.createElement("h1")
+    //     let text  = document.createElement("p")
+    //     let button = document.createElement("button")
+    //     let globe = document.createElement("img")
+
+    //     heroImg.src = hero.image
+
+    //     heroSection.appendChild(heroImg)
+        
+    //     button.appendChild(globe)
+    //     button.appendChild(document.createTextNode("Explore"))
+
+
+    // })
+
+
 
 
 }) // DOMContentLoaded slut
