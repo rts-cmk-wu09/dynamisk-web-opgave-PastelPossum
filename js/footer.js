9d6c528927d709e4531179070e76f6b5e40f6276
PastelPossum <iggyskywalker@gmail.com>
2023-02-15 09:13:39 +0100

    add footer data

---
+++ js/footer.js
@@ -0,0 +1,36 @@
+let footer = {
+
+    upperHead: "Easy Camper",
+    bottomHead: "When Passion Meets Comfort.",
+    links: [
+
+        {
+            headline: "Discover the Network",
+            link: "Where to shop" +
+            "Tour Guided" +
+            "Our Tours" +
+            "About Us"
+        },
+        {
+            headline: "Become a Manager",
+            link: "Join your Pairing" +
+            "Offer your Service" + 
+            "Are you a guide?"
+        },
+        {
+            headline: "Assistance",
+            link: "Support Center" +
+            "FAQs" +
+            "Cancallation Options" +
+            "Reliability and Sequrity"
+        }
+    ],
+    copyright: "EazyCamper. All rights reserved.",
+    footerNav: "About" +
+    "Site Map" + 
+    "Terms" + 
+    "Privacy Policy" + 
+    "Teams"
+
+
+}
