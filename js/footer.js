7bf6df97c6e677245a06f4167ccfb3350343f897
PastelPossum <iggyskywalker@gmail.com>
2023-02-16 11:17:09 +0100

    Add data to footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,61 +1,49 @@
-// let footer = {
+let footer = {
 
-//     upperHead: "Easy Camper",
-//     bottomText: "When Passion Meets Comfort.",
-//     links: [
+    upperHead: "Easy Camper",
+    bottomText: "When Passion Meets Comfort.",
+    links: [
 
-//         {
-//             footerHeadline: "Discover the Network",
-//             link: "Where to shop" +
-//                 "Tour Guided" +
-//                 "Our Tours" +
-//                 "About Us"
-//         },
-//         {
-//             footerHeadline: "Become a Manager",
-//             link: "Join your Pairing" +
-//                 "Offer your Service" +
-//                 "Are you a guide?"
-//         },
-//         {
-//             headline: "Assistance",
-//             link: "Support Center" +
-//                 "FAQs" +
-//                 "Cancallation Options" +
-//                 "Reliability and Sequrity"
-//         }
-//     ],
-//     copyright: "EazyCamper. All rights reserved.",
-//     footerNav: "About" +
-//         "Site Map" +
-//         "Terms" +
-//         "Privacy Policy" +
-//         "Teams"
-// }
+        {
+            headline: "Discover the Network",
+            linkOne: "Where to shop",
+            linkTwo: "Tour Guided",
+            linkThree: "Our Tours",
+            linkFour: "About Us"
+        },
 
-// // document.querySelector(".footer").innerHTML += `
-// // <section class="footer-aside">
-// // <h4 class="upper-head">${footer.upperHead}</h4>
-// // <p class="footer-text">${footer.bottomText}</p>
-// // </section>
-// // `
+        {
+            headline: "Become a Manager",
+             linkOne: "Join your Pairing" ,
+             linkTwo: "Offer your Service" ,
+             linkThree: "Are you a guide?",
+             linkFour: " "
+        },
 
-// let footSection = document.querySelector(".footer")
+        {
+            headline: "Assistance",
+                linkOne: "Support Center" ,
+                linkTwo: "FAQs" ,
+                linkThree: "Cancallation Options" ,
+                linkFour: "Reliability and Sequrity"
+        }
+    ],
 
-// let footHeader = document.createElement("h4")
-// footHeader.innerHTML = foot.upperHead;
-// footSection.append(footHeader)
+    copyright: "EazyCamper. All rights reserved.",
+    footLinkOne: "About",
+    footLinkTwo: "Site Map",
+    footLinkThree: "Terms",
+    footlinkFour: "Privacy Policy",
+    footLinkFive: "Teams"
+}
 
+let body = document.body
 
-// // footer.forEach((foot) => {
+let footerSection = document.createElement("section")
+footerSection.classList.add(".footer")
 
-// //     document.querySelector(".footer").innerHTML += `
-//     <section class="footer-cards"> 
-//     <h5 class="footer-card-h">${foot.links}</h5>
-//     <ul class="footer-links">
-//     <li>${foot.links}</li>
-//     </ul>
-//     </section>
-//     `
+let footerAside = document.createElement("section")
+footerAside.classList.add("footer-aside")
 
-// })
+body.append(footerSection)
+footerSection.append(footerAside)
